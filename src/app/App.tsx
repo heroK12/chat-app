e1229bdbf975c458c6888b4c52add5cc0fe6fc1d
heroK12 <koudai2517@gmail.com>
2023-04-13 23:36:33 +0900

    ✨ firebaseの導入

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
 import { ChatArea } from "../feature/message/ChatArea";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { getApp } from "firebase/app";
+import { initializeFirebaseApp } from "../lib/firebase/firebase";
 
+initializeFirebaseApp();
 function App() {
+    console.log(getApp());
     return (
         <div className="App">
             <Provider store={store}>
